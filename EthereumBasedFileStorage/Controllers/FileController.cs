c8c64062037c0b35ec76586d8055b86cc2784d4d
Tomheza <zemgtomas@gmail.com>
2020-02-15 23:29:01 +0200

    Add project files.

---
+++ EthereumBasedFileStorage/Controllers/FileController.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EthereumBasedFileStorage.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public File[] GetFiles()
+        {
+            var directory = new DirectoryInfo(@"C:\Users\zemgt\Downloads");
+            var files = directory.GetFiles();
+
+            return files.Select(f => new File
+            {
+                Name = f.Name,
+                DirectoryName = f.DirectoryName
+            }).ToArray();
+        }
+    }
+
+    public class File
+    {
+        public string Name { get; set; }
+        public string DirectoryName { get; set; }
+    }
+}
