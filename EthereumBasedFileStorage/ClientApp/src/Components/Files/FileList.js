d2fdf5b9fce6d2120caba73d1cfcb9f0d879b5d8
Tomheza <zemgtomas@gmail.com>
2020-02-18 08:10:27 +0200

    Changed view display from list to table

--- EthereumBasedFileStorage/ClientApp/src/Components/Files/FileList.js
+++ EthereumBasedFileStorage/ClientApp/src/Components/Files/FileList.js
@@ -7,13 +7,21 @@ import {
   ListItemSecondaryAction,
   Paper,
   Box,
-  Container
+  Container,
+  ListSubheader
 } from "@material-ui/core";
 import axios from "axios";
 import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
 import WorkIcon from "@material-ui/icons/Work";
-import DeleteIcon from "@material-ui/icons/Delete";
+import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+import More from "@material-ui/icons/MoreHoriz";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
 export default function FileList(props) {
   const [files, setFiles] = useState([]);
@@ -42,23 +50,31 @@ export default function FileList(props) {
     <Container>
       <Box m={3}>
         <Paper>
-          <List>
-            {files.map(file => (
-              <ListItem>
-                <ListItemAvatar>
-                  <Avatar>
-                    <WorkIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText primary={file.name}></ListItemText>
-                <ListItemSecondaryAction>
-                  <IconButton edge="end" aria-label="delete">
-                    <DeleteIcon />
-                  </IconButton>
-                </ListItemSecondaryAction>
-              </ListItem>
-            ))}
-          </List>
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table" title="Files">
+              <TableHead>
+                <TableRow>
+                  <TableCell>File Name</TableCell>
+                  <TableCell align="right">Modified</TableCell>
+                  <TableCell align="right">User</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {files.map(file => (
+                  <TableRow>
+                    <TableCell component="th" scope="row">
+                      {file.name}
+                    </TableCell>
+                    <TableCell align="right"></TableCell>
+                    <TableCell align="right"></TableCell>
+                    <TableCell align="right">
+                      <More />
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Paper>
       </Box>
     </Container>
